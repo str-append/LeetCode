4f6533190ca661e33df683cc0a40206a81b1edee
Yash Sharma <94126385+str-append@users.noreply.github.com>
2023-08-13 11:14:16 +0530

    Time: 137 ms (79.38%), Space: 83.7 MB (99.69%) - LeetHub

--- 2369-check-if-there-is-a-valid-partition-for-the-array/2369-check-if-there-is-a-valid-partition-for-the-array.cpp
+++ 2369-check-if-there-is-a-valid-partition-for-the-array/2369-check-if-there-is-a-valid-partition-for-the-array.cpp
@@ -1,24 +1,28 @@
 class Solution {
 public:
     bool validPartition(vector<int>& nums) {
-        vector<bool>dp(nums.size()+1,false);
-        dp[0] =true; //inital state
-        dp[1] = false; // no good subsequence can be formed with only single element
+        //space optimisation
+        bool a=true,b=false,c=false,curr=false;
         if(nums[0]==nums[1])
-        dp[2]=true; // if first 2 elements are equal then only dp[2] can be true
-        // curr state depends on only last 3 states
+        c=true;
         for(int i=3;i<=nums.size();i++){
+            curr=false;
             //2 consecutive elements are equal
-            if(nums[i-1]==nums[i-2] && dp[i-2]){ 
-                dp[i]=true;
-            }
+            if(nums[i-1]==nums[i-2] && b)
+                curr=true;
+
             // 3 consecutive elements are equal;
-            else if(nums[i-1]==nums[i-2] && nums[i-2]==nums[i-3] && dp[i-3])
-            dp[i]=true;
+            else if(nums[i-1]==nums[i-2] && nums[i-2]==nums[i-3] && a)
+                curr=true;
+
             //incresing sequence with difference 1
-            else if(nums[i-1]==nums[i-2]+1 && nums[i-2]==nums[i-3]+1 && dp[i-3])
-            dp[i]=true;
+            else if(i>=3 && nums[i-1]==nums[i-2]+1 && nums[i-2]==nums[i-3]+1 && a)
+                curr=true;
+
+            a=b;
+            b=c;
+            c=curr;
         }
-        return dp[nums.size()];
+        return c;
     }
 };
