e6bd5d46398019c2fffc7e25a17ecdfecf791d3b
Yash Sharma <94126385+str-append@users.noreply.github.com>
2023-08-13 16:08:23 +0530

    Time: 2 ms (96.38%), Space: 9.3 MB (6.58%) - LeetHub

--- 0120-triangle/0120-triangle.cpp
+++ 0120-triangle/0120-triangle.cpp
@@ -1,27 +1,31 @@
 class Solution {
 public:
+    //memoisation
+    int f(vector<vector<int>>& triangle,int i,int j,vector<vector<int>>&dp){
+        if(i==0 && j==0){
+            return dp[i][j] = triangle[i][j];
+        }
+        if(dp[i][j]!=INT_MAX)
+        return dp[i][j];
+        int a=INT_MAX,b=INT_MAX;
+        if(j>0)
+        a=f(triangle,i-1,j-1,dp);
+        if(j<triangle[i-1].size())
+        b=f(triangle,i-1,j,dp);
+        if(a==INT_MAX && b==INT_MAX)
+        a=0,b=0;
+        return dp[i][j] = min(a,b)+triangle[i][j];
+
+    }
     int minimumTotal(vector<vector<int>>& triangle) {
-        int m=0;
-        for(auto i:triangle){
-            m=max((int)i.size(),m);
-        }
-        vector<vector<int>>dp(triangle.size(),vector<int>(m,INT_MAX));
-        dp[0][0] = triangle[0][0];
-        for(int i=1;i<triangle.size();i++){
-            for(int j=0;j<triangle[i].size();j++){
-                if(i==0 && j==0){
-                    dp[i][j]=triangle[0][0];
-                }
-                else{
-                    int a=INT_MAX,b=INT_MAX;
-                    if(j>0)
-                    a=dp[i-1][j-1];
-                    if(j<=triangle[i-1].size())
-                    b=dp[i-1][j];
-                    dp[i][j] = min(a,b)+triangle[i][j];
-                }
-            }
-        }
-        return *min_element(dp[triangle.size()-1].begin(),dp[triangle.size()-1].end());
+       int m=0;
+       for(auto i:triangle)
+       m=max((int)i.size(),m);
+       int ans=INT_MAX;
+       vector<vector<int>>dp(triangle.size(),vector<int>(m,INT_MAX));
+       for(int i=0;i<m;i++){
+           ans = min(ans,f(triangle,triangle.size()-1,i,dp));
+       }
+       return ans;
     }
 };
