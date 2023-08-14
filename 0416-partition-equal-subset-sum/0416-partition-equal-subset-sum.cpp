eb66a4076737e65cde1df63360bd1e1606567679
Yash Sharma <94126385+str-append@users.noreply.github.com>
2023-08-14 14:58:21 +0530

    Time: 326 ms (55.47%), Space: 16.2 MB (60.44%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    bool f(vector<int>& nums, int ind,int cs, int target,vector<vector<int>>&dp){
+    bool memoisation(vector<int>& nums, int ind,int cs, int target,vector<vector<int>>&dp){
         if(cs==target){
             return true;
         }
@@ -9,17 +9,31 @@ public:
         if(dp[ind][cs]!=-1)
         return dp[ind][cs];
         bool take=false, notTake=false;
-        take = f(nums,ind+1,cs+nums[ind],target,dp);
-        notTake = f(nums,ind+1,cs,target,dp);
+        take = memoisation(nums,ind+1,cs+nums[ind],target,dp);
+        notTake = memoisation(nums,ind+1,cs,target,dp);
         return dp[ind][cs] = take|notTake;
     }
     bool canPartition(vector<int>& nums) {
+        //tabulation
         long long int sum = 0;
         sum = accumulate(nums.begin(),nums.end(),0);
         if(sum&1)
         return false;
-        vector<vector<int>>dp(nums.size()+1,vector<int>(sum,-1));
+        vector<vector<bool>>dp(nums.size()+1,vector<bool>(sum+1,false));
         sum=sum/2;
-        return f(nums,0,0,sum,dp);
+        for(int i=0;i<nums.size();i++){
+            dp[i][0]=true;
+        }
+        dp[0][nums[0]] = true;
+        for(int i=1;i<nums.size();i++){
+            for(int j=1;j<=sum;j++){
+                bool take=false, notTake=false;
+                if(j-nums[i]>=0)
+                take = dp[i-1][j-nums[i]];
+                notTake=dp[i-1][j];
+                dp[i][j] = take|notTake;
+            }
+        }
+        return dp[nums.size()-1][sum];
     }   
 };
